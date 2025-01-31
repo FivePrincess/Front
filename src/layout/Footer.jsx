5dfa5c58731538205438d4b7e36f286fae0da237
kwak <sal091625@gmail.com>
2025-01-31 22:19:32 +0900

    feat(#3): 버전에 따른 색상에 대한 변수 설정 수정

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="">@Copyright Five Princess</div>;
+  return <div className="">이 텍스트는 `purple-main` 색상으로 적용됩니다.</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,8 @@
-// tailwind.config.js
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,html}", // JS, TS, JSX, TSX 파일을 포함
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", "Helvetica", "Arial", "sans-serif"], // 기본 글꼴을 Inter로 설정
-      },
-      colors: {
-        "purple-dark": "#4A20E1",
-        "purple-main": "#7153DD",
-        "purple-medium": "#B9ABFF",
-        "purple-light": "#EAE4FF",
-
-        red: "#FF5D5A",
-        orange: "#FF7F00",
-        "yellow-main": "#FFC83A",
-        "yellow-light": "#FFEF5E",
-        "yellow-baby": "#FFFAD6",
-
-        black: "#000000",
-        "gray-main": "#747D8F",
-        "gray-stroke": "#C8D1E2",
-        "gray-light": "#F8F9FB",
-        white: "#FFFFFF",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
